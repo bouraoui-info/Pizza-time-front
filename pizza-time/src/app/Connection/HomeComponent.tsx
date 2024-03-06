8083765b39027a281dbe087af4a991e37a9298b1
Bouraoui abdallah <ingbouraouiabdallah@gmail.com>
2024-03-06 16:15:42 +0100

    mod

--- pizza-time/src/app/Connection/HomeComponent.tsx
+++ pizza-time/src/app/Connection/HomeComponent.tsx
@@ -1,17 +1,19 @@
-import { useEffect, useState } from 'react'
+ 'use client'
+import React, { useEffect, useState } from 'react'
 
 const HomeComponent = () => {
 
-const [data, setData] = useState(null)
-
-useEffect(() => {
-
-fetch('http://localhost:3001/api/users')
-.then((response) => response.json())
-.then((data) => setData(data))
-.catch(err => console.error('Error fetching data', err)); 
-}, []);
-
+// const [data, setData] = useState(null) 
+let dataq:any=null
+const getdata=()=>{
+  fetch('http://localhost:3001/api/users') // Make sure this URL matches your NestJS API endpoint
+  .then(response => response.json())
+  .then(data =>dataq=data)
+  .catch(error => console.error('Error:', error));}
+React.useEffect(() => {
+  getdata()
+}, [])
+console.log({dataq});
 
   return (
     <div>Home</div>
